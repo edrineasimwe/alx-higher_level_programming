f60729a47682d4367d7352b5cf3e7d5703ededa7
edrineasimwe <edrineasiimwe12@gmail.com>
2023-03-14 05:08:06 -0700

    three tasks

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+print("First 3 letters: Hol")
+print("Last 2 letters: on")
+print("Middle word: olberto")
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
