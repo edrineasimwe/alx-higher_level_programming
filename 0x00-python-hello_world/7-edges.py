83186ea36cdd7fb1eec196dfd7acc318c10e8a01
edrineasimwe <edrineasiimwe12@gmail.com>
2023-03-15 07:31:14 -0700

    my random chocolates

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print("First 3 letters: Hol")
-print("Last 2 letters: on")
-print("Middle word: olberto")
+print("First 3 letters: [:3]")
+print("Last 2 letters: [7:]")
+print("Middle word: [1:-1]")
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
