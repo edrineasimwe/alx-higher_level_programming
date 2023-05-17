08776cfe65849eaad82f0f69844a560f1d0fbbed
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-17 13:39:54 -0700

    SQL Project

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- This is a script that obtains the average of all scores in the table
 -- This query retrieves the average
-SELECT AVG(score) AS average from second_table;
+SELECT AVG(score) AS average FROM second_table;
 
