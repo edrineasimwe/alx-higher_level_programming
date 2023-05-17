50c8b0fe00b921cce9373249b4d6a0465522b4c6
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-17 16:30:48 -0700

    SQL Project

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,10 @@
 -- This script converts some database elements to utf8
 -- This chooses the database
 USE hbtn_0c_0;
+
+-- Alters the database settings or character encoding
+ALTER DATABASE hbtn_0c_0 DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
 -- This query converts the table to utf8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
