2e996a89315f99b32854c0701be4e0bf910b079a
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-17 13:38:03 -0700

    SQL Project

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
 -- This script updates Bob's marks to 10
 -- The following query does the trick
-UPDATE second_table set score = 10 where name = "Bob";
+UPDATE second_table SET score = 10 WHERE name = "Bob";
 
