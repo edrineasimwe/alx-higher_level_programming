9d69ac0fa179b021b14d01cd49a53be065447309
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-17 17:55:34 -0700

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- This is a script that creates the database hbtn_0d_usa and the table states
+-- The following vode should suffice
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Then we use the database
+USE hbtn_0d_usa;
+-- This creates the table
+CREATE TABLE IF NOT EXISTS states(
+id INT UNIQUE NOT NULL PRIMARY KEY,
+name VARCHAR(256) NOT NULL);
