8308e68b9445184b23ce1c131e684fa32d921190
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-17 17:26:35 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Here we are creating the user and their credentials
 CREATE USER IF NOT EXISTS'user_0d_2'@'localhost';
 -- Next we grant him all privileges
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1'@'localhost';
+USE hbtn_0d_2;
+GRANT SELECT ON * TO 'user_0d_1'@'localhost';
 -- Lastly we give them a password
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Then we flush privileges
