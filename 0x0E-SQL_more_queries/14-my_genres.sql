53ebe84afc1a8f1ea156d46456f2026ac69846a6
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-18 06:21:44 -0700

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,3 @@
+-- This script lists all genres contained in hbtn_0d_tvshow that belong to Dexter
+-- The following query does the trick
+SELECT TG.name AS name FROM tv_genres AS TG INNER JOIN tv_show_genres AS TSG ON TG.id = TSG.genre_id INNER JOIN tv_shows AS TS ON TS.id = TSG.show_id WHERE title = 'Dexter';
