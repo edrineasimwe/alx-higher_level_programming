1fdf36c12a60427c24493c6ffd9b8f2669ffaec3
RafeAaron16 <rafedrew7@gmail.com>
2023-06-14 11:04:39 +0300

    I'm now somewhere

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2]) {
   if (Number(process.argv[2])) {
-    console.log('My number ' + process.argv[2]);
+    console.log('My number: ' + process.argv[2]);
   } else {
     console.log('Not a number');
   }
