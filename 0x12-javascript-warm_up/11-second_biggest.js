19df40e84b6e9e99f426b7a4d0ad274c77b59b2c
RafeAaron16 <rafedrew7@gmail.com>
2023-06-14 12:52:55 +0300

    Fix for 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,15 @@
 if (!process.argv[2] || !process.argv[3]) console.log(0);
 else {
   let biggest = 0;
+  let second_biggest = 0;
   for (let i = 0; ;i++) {
     if (!process.argv[i]) break;
 
     if (Number(process.argv[i]) > biggest) {
+      second_biggest = biggest;
       biggest = Number(process.argv[i]);
     }
   }
 
-  console.log(biggest);
+  console.log(second_biggest);
 }
