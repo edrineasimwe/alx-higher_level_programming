4ebdcb024c78f35e878140ee0e279467beb0fc5d
RafeAaron16 <rafedrew7@gmail.com>
2023-06-14 13:15:58 +0300

    Something

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,16 +2,6 @@
 
 if (!process.argv[2] || !process.argv[3]) console.log(0);
 else {
-  let biggest = 0;
-  let second_biggest = 0;
-  for (let i = 0; ;i++) {
-    if (!process.argv[i]) break;
-
-    if (Number(process.argv[i]) > biggest) {
-      second_biggest = biggest;
-      biggest = Number(process.argv[i]);
-    }
-  }
-
-  console.log(second_biggest);
+	const arraySort = (process.argv.slice(2)).sort();
+	console.log(arraySort[process.argv.length - 5]);
 }
