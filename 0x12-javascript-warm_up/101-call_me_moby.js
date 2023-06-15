3eb15254036c1e65bcd3ed9299cc578912690507
RafeAaron16 <rafedrew7@gmail.com>
2023-06-15 12:37:33 +0300

    Call Me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
+exports.callMeMoby = function (number, theFunction) {
+  for(let i = 0; i < number; i++){
+      theFunction();
+  }
 };
